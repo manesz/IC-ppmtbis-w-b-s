138bbf7c6a8807244d10f24db212e4858f8294fa
chukkapun <ruxchuk@gmail.com>
2013-04-24 17:00:31 +0700

    add code

---
+++ application/controllers/client.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Chukkapun
+ * Date: 23/4/2556
+ * Time: 23:21 น.
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Client extends CI_Controller
+{
+
+    public function clientList()
+    {
+
+        $this->load->view('client-list');
+    }
+    public function clientNew()
+    {
+
+        $this->load->view('client-new');
+    }
+    public function clientEdit()
+    {
+
+        $this->load->view('client-edit');
+    }
+
+}
