d3c0c6f25d804e7dc5ae2c920bc7e6cdd9631490
Chukkapun <ruxchuk@gmail.com>
2013-05-18 16:48:42 +0700

    set path

---
+++ application/views/website/header.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en-US" class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en-US" class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-US" class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-US" class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en-US"> <!--<![endif]-->
+
+<head>
+    <!--
+
+    Theme:      Freshlook HTML Vs1.0.3
+    Author:		Designare
+    Support:    http://support.designarethemes.com/
+    Portfolio:  http://themeforest.net/user/Designare
+
+    -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Freshlook | Clean &amp; Fresh HTML</title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+
+    <meta name="description" content="">
+
+    <!-- Place favicon.ico and apple-touch-icons in the images folder -->
+    <!-- favicon -->
+    <link rel="shortcut icon" href="favicon.png">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <!--60X60-->
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-ipad.png">
+    <!--72X72-->
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-iphone4.png">
+    <!--114X114-->
+    <link rel="apple-touch-icon" sizes="144x144" href="images/apple-touch-icon-ipad3.png">
+    <!--144X144-->
+
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!--[if IE 8]>
+    <link rel="stylesheet" href="css/ieeight.css" type="text/css" media="screen, projection"/>
+    <![endif]-->
+
+    <!--[if IE 9]>
+    <link rel="stylesheet" href="css/ienine.css" type="text/css" media="screen, projection"/>
+    <![endif]-->
+
+    <!-- Freshlook CSS -->
+    <link rel='stylesheet' href='css/skeleton.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/reset.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/default.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/camera.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/da-thumbs.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/tango/skin.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/flexslider.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/superfish.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/blog.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/projects.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/prettyPhoto.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/resize.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/tweet.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/farbtastic.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/shortcodes.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/captions-original.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/captions.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/settings.css' type='text/css' media='all'/>
+    <link rel='stylesheet' href='css/mediaelementplayer.css' type='text/css' media='all'/>
+
+
+    <!-- Freshlook Scripts -->
+    <script type='text/javascript' src='js/jquery.1.8.3.js'></script>
+    <script type='text/javascript' src='js/slides.min.jquery.js'></script>
+    <script type='text/javascript' src='js/jquery.jcarousel.min.js'></script>
+    <script type='text/javascript' src='js/mediaelement-and-player.min.js'></script>
+    <script type='text/javascript' src='js/jquery.mobile.customized.min.js'></script>
+    <script type='text/javascript' src='js/jquery.easing.1.3.js'></script>
+    <script type='text/javascript' src='js/modernizr.custom.79639.js'></script>
+    <script type='text/javascript' src='js/camera.min.js'></script>
+    <script type='text/javascript' src='js/jquery.themepunch.revolution.js'></script>
+    <script type='text/javascript' src='js/jquery.themepunch.plugins.min.js'></script>
+    <script type='text/javascript' src='js/tabs.js'></script>
+    <script type='text/javascript' src='js/jquery.flexslider-min.js'></script>
+    <script type='text/javascript' src='js/jquery.prettyPhoto.js'></script>
+    <script type='text/javascript' src='js/freshlook.js'></script>
+    <script type='text/javascript' src='js/jquery.hoverdir.js'></script>
+    <script type='text/javascript' src='js/jquery.cycle.all.js'></script>
+    <script type='text/javascript' src='js/jQuerytools.js'></script>
+    <script type='text/javascript' src='js/superfish.js'></script>
+    <script type='text/javascript' src='js/supersubs.js'></script>
+    <script type='text/javascript' src='js/jflickrfeed.js'></script>
+    <script type='text/javascript' src='http://twitter.com/javascripts/blogger.js'></script>
+    <script type='text/javascript' src='js/jquery.quicksand.js'></script>
+    <script type='text/javascript' src='http://maps.google.com/maps/api/js?sensor=false'></script>
+    <script type='text/javascript' src='js/contact.js'></script>
+    <script type='text/javascript' src='js/jquery.scrolltotop.js'></script>
+    <script type='text/javascript' src='js/jquery.tweet.js'></script>
+    <script type='text/javascript' src='js/farbtastic.js'></script>
+    <script type='text/javascript' src='js/jquery.xcolor.js'></script>
+
+</head>
+
+<body class="home page">
+<div class="everything">
+
+<!-- COLOR Switcher (hidden by default) -->
+<div id="option_wrapper" style="display: none;">
+
+    <div class="inner">
+        <div id="colorpicker"></div>
+        <form style="margin: 20px 20px 0 28px;position: relative;top:12px;">
+            <input type="text" id="color" name="color" value="#7AB317"/>
+        </form>
+    </div>
+
+</div>
+
+<div class="option_btn settings-close" style="display: none;"></div>
+<!-- END OF COLOR Switcher -->
+
+<!-- START: HEADER -->
+<div class="header_container">
+<header id="header" class="container">
+
+<div class="logo_and_menu">
+
+<!-- START: LOGO & SLOGAN -->
+<div class="logo five columns">
+    <a href="index.html" tabindex="-1" style="text-indent: 0px;">
+        <h1 class="logo" style="top: 24px; left: 0px; background: none; font-size: 12px;">Fresh<span class=color_logo>look</span>
+        </h1>
+    </a>
+
+    <div class="slogan"
+         style="background: none; font-family: 'Georgia'; font-style: italic; font-size: 12px; color: #cccccc;">Clean
+        &amp; Fresh HTML
+    </div>
+</div>
+
+<nav class="menu eleven columns">
+    <?php include "nav.php"?>
+</nav>
+
+</div>
+</header>
+<!-- HEADER SHADOW -->
+<div class="header-shadow"></div>
+</div>
+<!-- END: HEADER -->
+
+<!-- START: INFO ABOVE MENU
+<div class="info_above_menu">
+    <div class="container">
+        <div class="info_above_menu_left eight columns">
+            Awesome html5 theme by Designare. <a href="http://themeforest.net/item/freshlook-responsive-multipurpose-html5-template/4452404?ref=Designare">Buy it on Themeforest  →</a>
+        </div>
+        <div class="info_above_menu_right eight columns">
+            Email: <a href=mailto:geral@designarethemes.com> geral@designarethemes.com </a> &nbsp; | &nbsp; Tel: +351 966 666 666
+        </div>
+    </div>
+</div>
+-->
+<!-- END: INFO ABOVE MENU -->
+
+<!-- START: CAMERA SLIDER -->
+<div id="slider_container">
+    <?php include "front-image-slide.php"?>
+</div>
+<!-- END: CAMERA SLIDER -->
+
