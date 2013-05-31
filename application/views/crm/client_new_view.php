b72f837466b4482a72d766c6d2fa9e2dff131415
Chukkapun <ruxchuk@gmail.com>
2013-05-31 15:18:39 +0700

    แก้ add client

--- application/views/crm/client_new_view.php
+++ application/views/crm/client_new_view.php
@@ -23,8 +23,20 @@ $baseUrl = base_url();
         border: none;
         padding: 0;
     }
+
     .uploadify:hover .bt-class {
         background-color: transparent;
+    }
+
+    .uploadify-queue-item {
+        background-color: #C7FFFF;
+        -webkit-border-radius: 3px;
+        -moz-border-radius: 3px;
+        border-radius: 3px;
+        font: 11px Verdana, Geneva, sans-serif;
+        margin-top: 5px;
+        width: 280px;
+        padding: 10px;
     }
 </style>
 <script>
@@ -38,23 +50,31 @@ $baseUrl = base_url();
         genUploadImage("#logo_image_path", "#image_show", "#logo_image");
         genUploadFiles("#file_upload", "", "");
     });
+
+    var numImageSelect = 0;
+    var numFileSelect = 0;
     function genUploadImage(btnUpload, idReload, idSave) {
         $(btnUpload).uploadify({
-            'multi'    : false,
+            'multi': false,
 //            'method'   : 'post',
             'auto': false,
-            'formData'      : {
-                'folder_id' : "noSet",
-                'file_type' : "2",//image
+            'formData': {
+                'folder_id': "noSet",
+                'file_type': "2",//image
                 'path_upload': pathImageUpload
             },
-            'onUploadStart' : function(file)
-            {
-                $(btnUpload).uploadify('settings', 'formData',{
-                    'folder_id' : folderID,
-                    'file_type' : "2",
-                    'path_upload' : pathImageUpload
+            'onUploadStart': function (file) {
+                $(btnUpload).uploadify('settings', 'formData', {
+                    'folder_id': folderID,
+                    'file_type': "2",
+                    'path_upload': pathImageUpload
                 });
+            },
+            'onSelect': function (file) {
+                numImageSelect++;
+            },
+            'onCancel': function (file) {
+                numImageSelect--;
             },
             'swf': swfPath,
             'uploader': pathUploadify,
@@ -66,15 +86,19 @@ $baseUrl = base_url();
                 alert('Flash was not detected.');// detect flash compatible
             }, 'onUploadSuccess': function (file, data, response) {
                 $.post(url_update_image_client, {
-                        "id":folderID,
-                        "path":data
+                        "id": folderID,
+                        "path": data
                     },
                     function (result) {
                         if (result == "update fail") {
                             alert('เกิดการผิดพลาด\n** กรุณาตรวจสอบ **');
                         } else {
                             //window.location.reload();
-                            $('#file_upload').uploadify('upload', '*');
+                            if (numFileSelect > 0) {
+                                $('#file_upload').uploadify('upload', '*');
+                            } else {
+                                window.location.reload();
+                            }
                         }
                     }
                 );
@@ -83,26 +107,29 @@ $baseUrl = base_url();
         });
     }
 
-    function genUploadFiles(btnUpload, idReload, idSave)
-    {
+    function genUploadFiles(btnUpload, idReload, idSave) {
         $(btnUpload).uploadify({
 //            'multi'    : false,
 //            'method'   : 'post',
-            //'buttonImage' : "<?php echo $baseUrl; ?>assets/plugin/uploadify/browse-btn.png",
-            'buttonClass' : 'bt-class',
+            'buttonClass': 'bt-class',
             'auto': false,
-            'formData'      : {
-                'folder_id' : "noSet",
-                'file_type' : "1",//file
+            'formData': {
+                'folder_id': "noSet",
+                'file_type': "1",//file
                 'path_upload': pathFileUpload
             },
-            'onUploadStart' : function(file)
-            {
-                $(btnUpload).uploadify('settings', 'formData',{
-                    'folder_id' : folderID,
-                    'file_type' : "1",
-                    'path_upload' : pathFileUpload
+            'onUploadStart': function (file) {
+                $(btnUpload).uploadify('settings', 'formData', {
+                    'folder_id': folderID,
+                    'file_type': "1",
+                    'path_upload': pathFileUpload
                 });
+            },
+            'onSelect': function (file) {
+                numFileSelect++;
+            },
+            'onCancel': function (file) {
+                numFileSelect--;
             },
             'swf': swfPath,
             'uploader': pathUploadify,
@@ -113,7 +140,8 @@ $baseUrl = base_url();
             'onFallback': function () {
                 alert('Flash was not detected.');// detect flash compatible
             }, 'onUploadSuccess': function (file, data, response) {
-                alert(data);
+                window.location.reload();
+                //alert(data);
             },
             'queueSizeLimit': 5
         });
@@ -137,7 +165,15 @@ $baseUrl = base_url();
                             alert('เกิดการผิดพลาด\n** กรุณาตรวจสอบ **');
                         } else {
                             folderID = result;
-                            $('#logo_image_path').uploadify('upload', '*');
+                            if (numImageSelect > 0) {
+                                $('#logo_image_path').uploadify('upload', '*');
+                            } else {
+                                if (numFileSelect > 0) {
+                                    $('#file_upload').uploadify('upload', '*');
+                                } else {
+                                    window.location.reload();
+                                }
+                            }
                         }
                     }
                 );
@@ -183,40 +219,58 @@ $baseUrl = base_url();
         <div class="block-content collapse in">
             <form id="formPost" name="formPost" method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
                 <input type="hidden" name="key_account_manager_id" value="1"/>
+
                 <div class="row-fluid">
                     <div class="span2">รูปบริษัท</div>
                     <div class="span4">
                         <input name="userfile" type="file" id="logo_image_path"/>
                         <input name="logo_image" type="hidden" id="logo_image" value=""/>
-                    </div><!--  class="input-block-level" -->
-                    <div class="span2">ชื่อบริษัทภาษาไทย </div>
-                    <div class="span4"><input name="name_th" type="text" id="name_th" class="input-block-level"/></div><!--  class="input-block-level" -->
+                    </div>
+                    <!--  class="input-block-level" -->
+                    <div class="span2">ชื่อบริษัทภาษาไทย</div>
+                    <div class="span4"><input name="name_th" type="text" id="name_th" class="input-block-level"/></div>
+                    <!--  class="input-block-level" -->
                 </div>
                 <div class="row-fluid">
-                    <div class="span2">ชื่อบริษัทแบบย่อ </div>
-                    <div class="span4"><input name="name_short" type="text" id="name_short" class="input-block-level"/></div><!--  class="input-block-level" -->
+                    <div class="span2">ชื่อบริษัทแบบย่อ</div>
+                    <div class="span4"><input name="name_short" type="text" id="name_short" class="input-block-level"/>
+                    </div>
+                    <!--  class="input-block-level" -->
 
-                    <div class="span2">ชื่อบริษัทภาษาอังกฤษ </div>
-                    <div class="span4"><input name="name_en" type="text" id="name_en" class="input-block-level"/></div><!--  class="input-block-level" -->
+                    <div class="span2">ชื่อบริษัทภาษาอังกฤษ</div>
+                    <div class="span4"><input name="name_en" type="text" id="name_en" class="input-block-level"/></div>
+                    <!--  class="input-block-level" -->
                 </div>
                 <div class="row-fluid">
                     <div class="span2">รายละเอียดบริษัทภาษาไทย</div>
-                    <div class="span4"><textarea id="description_th" class="input-block-level" rows="5" name="description_th"></textarea></div><!--  class="input-block-level" -->
-                    <div class="span2">รายละเอียดบริษัทภาษาอังกฤษ </div>
-                    <div class="span4"><textarea name="description_en" id="description_en" class="input-block-level" rows="5"></textarea></div><!--  class="input-block-level" -->
+                    <div class="span4"><textarea id="description_th" class="input-block-level" rows="5"
+                                                 name="description_th"></textarea></div>
+                    <!--  class="input-block-level" -->
+                    <div class="span2">รายละเอียดบริษัทภาษาอังกฤษ</div>
+                    <div class="span4"><textarea name="description_en" id="description_en" class="input-block-level"
+                                                 rows="5"></textarea></div>
+                    <!--  class="input-block-level" -->
 
                 </div>
                 <div class="row-fluid">
                     <div class="span2">ที่อยู่บริษัทภาษาไทย</div>
-                    <div class="span4"><textarea name="address_th" id="address_th" class="input-block-level" rows="5"></textarea></div><!--  class="input-block-level" -->
-                    <div class="span2">ที่อยู่ภาษาอังกฤษ </div>
-                    <div class="span4"><textarea name="address_en" id="address_en" class="input-block-level" rows="5"></textarea></div><!--  class="input-block-level" -->
+                    <div class="span4"><textarea name="address_th" id="address_th" class="input-block-level"
+                                                 rows="5"></textarea></div>
+                    <!--  class="input-block-level" -->
+                    <div class="span2">ที่อยู่ภาษาอังกฤษ</div>
+                    <div class="span4"><textarea name="address_en" id="address_en" class="input-block-level"
+                                                 rows="5"></textarea></div>
+                    <!--  class="input-block-level" -->
                 </div>
                 <div class="row-fluid">
                     <div class="span2">เบอร์โทรศัพท์ Office</div>
-                    <div class="span4"><input name="office_number" type="text" id="office_number" class="input-block-level"/></div><!--  class="input-block-level" -->
-                    <div class="span2">Email Office </div>
-                    <div class="span4"><input name="email_office" type="text" id="email_office" class="input-block-level"/></div><!--  class="input-block-level" -->
+                    <div class="span4"><input name="office_number" type="text" id="office_number"
+                                              class="input-block-level"/></div>
+                    <!--  class="input-block-level" -->
+                    <div class="span2">Email Office</div>
+                    <div class="span4"><input name="email_office" type="text" id="email_office"
+                                              class="input-block-level"/></div>
+                    <!--  class="input-block-level" -->
                 </div>
                 <div class="row-fluid">
                     <div class="span2">ประเภทบริษัท</div>
@@ -228,32 +282,49 @@ $baseUrl = base_url();
                             }
                             ?>
                         </select>
-                    </div><!--  class="input-block-level" -->
+                    </div>
+                    <!--  class="input-block-level" -->
                     <div class="span2">เบอร์ FAX</div>
-                    <div class="span4"><input name="fax_number" type="text" id="fax_number" class="input-block-level"/></div><!--  class="input-block-level" -->
+                    <div class="span4"><input name="fax_number" type="text" id="fax_number" class="input-block-level"/>
+                    </div>
+                    <!--  class="input-block-level" -->
                 </div>
                 <div class="row-fluid">
-                    <div class="span2">รายละเอียดสินค้าและบริการภาษาไทย </div>
-                    <div class="span4"><textarea name="main_product_th" id="main_product_th" class="input-block-level" rows="5"></textarea></div><!--  class="input-block-level" -->
+                    <div class="span2">รายละเอียดสินค้าและบริการภาษาไทย</div>
+                    <div class="span4"><textarea name="main_product_th" id="main_product_th" class="input-block-level"
+                                                 rows="5"></textarea></div>
+                    <!--  class="input-block-level" -->
                     <div class="span2">รายละเอียดสินค้าและบริการภาษาอังกฤษ</div>
-                    <div class="span4"><textarea name="main_product_en" id="main_product_en" class="input-block-level" rows="5"></textarea></div><!--  class="input-block-level" -->
+                    <div class="span4"><textarea name="main_product_en" id="main_product_en" class="input-block-level"
+                                                 rows="5"></textarea></div>
+                    <!--  class="input-block-level" -->
                 </div>
                 <div class="row-fluid">
-                    <div class="span2">link website </div>
-                    <div class="span4"><input name="website_link" type="text" id="website_link" class="input-block-level"/></div><!--  class="input-block-level" -->
+                    <div class="span2">link website</div>
+                    <div class="span4"><input name="website_link" type="text" id="website_link"
+                                              class="input-block-level"/></div>
+                    <!--  class="input-block-level" -->
                     <div class="span2">เลือกเอกสาร</div>
-                    <div class="span4"><input type="file" id="file_upload" name="file_upload"/></div><!--  class="input-block-level" -->
+                    <div class="span4"><input type="file" id="file_upload" name="file_upload"/></div>
+                    <!--  class="input-block-level" -->
                 </div>
                 <div class="row-fluid">
-                    <div class="span2">Remark </div>
-                    <div class="span10"><textarea name="remark" id="remark" class="input-block-level" rows="5"></textarea></div><!--  class="input-block-level" -->
+                    <div class="span2">Remark</div>
+                    <div class="span10"><textarea name="remark" id="remark" class="input-block-level"
+                                                  rows="5"></textarea></div>
+                    <!--  class="input-block-level" -->
                 </div>
                 <p><strong>รายละเอียดการจ่ายเงิน</strong></p>
+
                 <div class="row-fluid">
-                    <div class="span2">Recruitment fee </div>
-                    <div class="span4"><input name="recruitment_fee" type="text" id="recruitment_fee" class="input-block-level"/></div><!--  class="input-block-level" -->
-                    <div class="span2">Payment Term </div>
-                    <div class="span4"><input name="payment_term" type="text" id="payment_term" class="input-block-level"/></div><!--  class="input-block-level" -->
+                    <div class="span2">Recruitment fee</div>
+                    <div class="span4"><input name="recruitment_fee" type="text" id="recruitment_fee"
+                                              class="input-block-level"/></div>
+                    <!--  class="input-block-level" -->
+                    <div class="span2">Payment Term</div>
+                    <div class="span4"><input name="payment_term" type="text" id="payment_term"
+                                              class="input-block-level"/></div>
+                    <!--  class="input-block-level" -->
                 </div>
                 <div class="row-fluid">
                     <div class="span12" align="right">
