70372ab4bbd0206a4649e1b89ae7f782121ec3b0
Chukkapun <ruxchuk@gmail.com>
2013-06-13 13:35:41 +0700

    slide frontend

--- application/views/website/front-image-slide.php
+++ application/views/website/front-image-slide.php
@@ -1,147 +1,182 @@
-<!-- SLIDE #01 -->
-<div data-thumb="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg"
-     data-src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg">
-    <div class="camera_caption">
-        <div class="container" style="display:none; opacity: 0; filter: alpha(opacity=0);">
+<?php
 
-            <div class="eight columns" style="text-align: left; height: 100%;">
+$arrSlide = $this->CPanel_model->slideList();
+$baseUrl = base_url();
+?>
 
-                <div class="title des_moveFromTop"><h1>Clean &amp; Fresh Theme for Business, Agency or Creative
-                    Portfolios</h1></div>
+<?php foreach ($arrSlide as $key => $value) :
+    $urlImageSlide = $baseUrl . "upload/images/slide/" . $value->id . "/";
+    ?>
+    <div data-thumb="<?php echo $urlImageSlide . $value->image; ?>"
+         data-src="<?php echo $urlImageSlide . $value->image; ?>">
+        <div class="camera_caption">
+            <div class="container" style="display:none;; opacity: 0; filter: alpha(opacity=0);">
 
-                <div class="text des_moveFromBottom">Freshlook is the ideal solution for your business, Flexible
-                    &amp; Responsive (works on all devices, including mobile based), it comes with 50+ Visual
-                    Shortcodes, Unlimited Portfolios so you can present your products properly.
+                <div class="eight columns" style="text-align: left; height: 100%;">
+
+                    <div class="title des_moveFromTop"><h1><?php echo $value->title; ?></h1></div>
+
+                    <div class="text des_moveFromBottom"><?php echo $value->description; ?></div>
+
+                    <a class="button medium yellow des_moveFromBottom"
+                       href="#">Read more</a>
                 </div>
 
-                <a class="button medium yellow des_moveFromBottom"
-                   href="http://designarethemes.com/themes/freshlookwp/freshlook-features/">Read more</a>
-            </div>
-
-            <div class="eight columns" style="height: 100%;">
-                <img class="image des_moveFromBottom"
-                     src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/gril12.png"
-                     alt="">
+                <div class="eight columns" style="height: 100%;">
+                    <img class="image des_moveFromBottom"
+                         src="<?php echo $urlImageSlide . $value->image_2; ?>" alt="">
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+<?php endforeach; ?>
+
+
+<!-- SLIDE #01 -->
+<!--<div data-thumb="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg"-->
+<!--     data-src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg">-->
+<!--    <div class="camera_caption">-->
+<!--        <div class="container" style="display:none; opacity: 0; filter: alpha(opacity=0);">-->
+<!---->
+<!--            <div class="eight columns" style="text-align: left; height: 100%;">-->
+<!---->
+<!--                <div class="title des_moveFromTop"><h1>Clean &amp; Fresh Theme for Business, Agency or Creative-->
+<!--                        Portfolios</h1></div>-->
+<!---->
+<!--                <div class="text des_moveFromBottom">Freshlook is the ideal solution for your business, Flexible-->
+<!--                    &amp; Responsive (works on all devices, including mobile based), it comes with 50+ Visual-->
+<!--                    Shortcodes, Unlimited Portfolios so you can present your products properly.-->
+<!--                </div>-->
+<!---->
+<!--                <a class="button medium yellow des_moveFromBottom"-->
+<!--                   href="http://designarethemes.com/themes/freshlookwp/freshlook-features/">Read more</a>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="eight columns" style="height: 100%;">-->
+<!--                <img class="image des_moveFromBottom"-->
+<!--                     src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/gril12.png"-->
+<!--                     alt="">-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 
 <!-- SLIDE #02 -->
-<div data-thumb="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/1353613773slid6.jpg"
-     data-src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/1353613773slid6.jpg">
-    <div class="camera_caption">
-        <div class="container" style="display:none; opacity: 0; filter: alpha(opacity=0);">
-
-
-            <div class="eight columns" style="text-align: left; height: 100%;">
-
-                <div class="title des_moveFromRight"><h1>Freshlook come with two layouts styles! Boxed and
-                    Fullwith.</h1></div>
-
-                <div class="text des_moveFromLeft">You can set all your site layout with the boxed or fullwith
-                    style, but if you want both styles in same time in your site you can use the page options to
-                    customize each page individually.
-                </div>
-
-                <a class="button medium green des_moveFromRight"
-                   href="http://designarethemes.com/themes/freshlook-html/home-boxed.html">View Home Boxed</a>
-            </div>
-
-            <div class="eight columns" style="height: 100%;">
-                <img class="image des_moveFromTop"
-                     src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/img_desc6.png"
-                     alt="">
-            </div>
-        </div>
-    </div>
-</div>
+<!--<div data-thumb="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/1353613773slid6.jpg"-->
+<!--     data-src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/1353613773slid6.jpg">-->
+<!--    <div class="camera_caption">-->
+<!--        <div class="container" style="display:none; opacity: 0; filter: alpha(opacity=0);">-->
+<!---->
+<!---->
+<!--            <div class="eight columns" style="text-align: left; height: 100%;">-->
+<!---->
+<!--                <div class="title des_moveFromRight"><h1>Freshlook come with two layouts styles! Boxed and-->
+<!--                    Fullwith.</h1></div>-->
+<!---->
+<!--                <div class="text des_moveFromLeft">You can set all your site layout with the boxed or fullwith-->
+<!--                    style, but if you want both styles in same time in your site you can use the page options to-->
+<!--                    customize each page individually.-->
+<!--                </div>-->
+<!---->
+<!--                <a class="button medium green des_moveFromRight"-->
+<!--                   href="http://designarethemes.com/themes/freshlook-html/home-boxed.html">View Home Boxed</a>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="eight columns" style="height: 100%;">-->
+<!--                <img class="image des_moveFromTop"-->
+<!--                     src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/img_desc6.png"-->
+<!--                     alt="">-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 
 <!-- SLIDE #03 -->
-<div data-thumb="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg"
-     data-src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg">
-    <div class="camera_caption">
-        <div class="container" style="display:none; opacity: 0; filter: alpha(opacity=0);">
-
-            <div class="eight columns" style="text-align: left; height: 100%;">
-
-                <div class="title des_moveFromBottom"><h1>This theme is Ultra Responsive &amp; Full Mobile ready.
-                    Try it!</h1></div>
-
-                <div class="text des_moveFromBottom">Try resizing your browser’s screen and see as the layout
-                    adjusts to the size of the window.It just looks fantastic on mobile devices such as
-                    iPhones/Smarphones or Tablet-Pc’s.
-                </div>
-
-                <a class="button medium green des_moveFromBottom"
-                   href="http://themeforest.net/item/freshlook-responsive-multipurpose-wp-theme/3411374?ref=Designare">Buy
-                    it Now</a>
-            </div>
-
-            <div class="eight columns" style="height: 100%;">
-                <img class="image des_fade"
-                     src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/devicess.png"
-                     alt="">
-            </div>
-        </div>
-    </div>
-</div>
+<!--<div data-thumb="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg"-->
+<!--     data-src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg">-->
+<!--    <div class="camera_caption">-->
+<!--        <div class="container" style="display:none; opacity: 0; filter: alpha(opacity=0);">-->
+<!---->
+<!--            <div class="eight columns" style="text-align: left; height: 100%;">-->
+<!---->
+<!--                <div class="title des_moveFromBottom"><h1>This theme is Ultra Responsive &amp; Full Mobile ready.-->
+<!--                    Try it!</h1></div>-->
+<!---->
+<!--                <div class="text des_moveFromBottom">Try resizing your browser’s screen and see as the layout-->
+<!--                    adjusts to the size of the window.It just looks fantastic on mobile devices such as-->
+<!--                    iPhones/Smarphones or Tablet-Pc’s.-->
+<!--                </div>-->
+<!---->
+<!--                <a class="button medium green des_moveFromBottom"-->
+<!--                   href="http://themeforest.net/item/freshlook-responsive-multipurpose-wp-theme/3411374?ref=Designare">Buy-->
+<!--                    it Now</a>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="eight columns" style="height: 100%;">-->
+<!--                <img class="image des_fade"-->
+<!--                     src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/devicess.png"-->
+<!--                     alt="">-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 
 <!-- SLIDE #04 -->
-<div data-thumb="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid1.jpg"
-     data-src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid1.jpg">
-    <div class="camera_caption">
-        <div class="container" style="display:none; opacity: 0; filter: alpha(opacity=0);">
-
-            <div class="eight columns" style="text-align: left; height: 100%;">
-
-                <div class="title des_moveFromLeft"><h1>Powerful Slider with Beautiful and catchy transitions
-                    effects.</h1></div>
-
-                <div class="text des_moveFromRight">With Freshlook’s powerful main slider your clients will be
-                    stunned. Capable of dealing with both Photos and Video, it has beautiful and catchy transition
-                    effects, thumbnail previews and animated captions!
-                </div>
-
-                <a class="button medium green des_moveFromRight"
-                   href="http://themeforest.net/item/freshlook-responsive-multipurpose-wp-theme/3411374?ref=Designare">Buy
-                    it now!</a>
-            </div>
-
-            <div class="eight columns" style="height: 100%;">
-                <img class="image des_moveFromLeft"
-                     src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/img_desc31.png"
-                     alt="">
-            </div>
-        </div>
-    </div>
-</div>
+<!--<div data-thumb="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid1.jpg"-->
+<!--     data-src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid1.jpg">-->
+<!--    <div class="camera_caption">-->
+<!--        <div class="container" style="display:none; opacity: 0; filter: alpha(opacity=0);">-->
+<!---->
+<!--            <div class="eight columns" style="text-align: left; height: 100%;">-->
+<!---->
+<!--                <div class="title des_moveFromLeft"><h1>Powerful Slider with Beautiful and catchy transitions-->
+<!--                    effects.</h1></div>-->
+<!---->
+<!--                <div class="text des_moveFromRight">With Freshlook’s powerful main slider your clients will be-->
+<!--                    stunned. Capable of dealing with both Photos and Video, it has beautiful and catchy transition-->
+<!--                    effects, thumbnail previews and animated captions!-->
+<!--                </div>-->
+<!---->
+<!--                <a class="button medium green des_moveFromRight"-->
+<!--                   href="http://themeforest.net/item/freshlook-responsive-multipurpose-wp-theme/3411374?ref=Designare">Buy-->
+<!--                    it now!</a>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="eight columns" style="height: 100%;">-->
+<!--                <img class="image des_moveFromLeft"-->
+<!--                     src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/img_desc31.png"-->
+<!--                     alt="">-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 
 <!-- SLIDE #05 -->
-<div data-thumb="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg"
-     data-src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg">
-    <div class="camera_caption">
-        <div class="container" style="display:none; opacity: 0; filter: alpha(opacity=0);">
-            <div class="eight columns" style="text-align: left; height: 100%;">
-
-                <div class="title des_moveFromLeft"><h1>Customize colors, backgrounds, fonts... Style every page
-                    individually!</h1></div>
-
-                <div class="text des_moveFromRight">Build your page with an incredible ease using the Freshlook Page
-                    Options, where you can customize every page individually, like adding Slides, change
-                    backgrounds… you name it!.
-                </div>
-
-                <a class="button medium yellow des_moveFromRight"
-                   href="http://themeforest.net/item/freshlook-responsive-multipurpose-wp-theme/3411374?ref=Designare">Like
-                    this theme?</a>
-            </div>
-
-            <div class="eight columns" style="height: 100%;">
-                <img class="image des_moveFromLeft"
-                     src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/fresh-colors-1.png"
-                     alt="">
-            </div>
-        </div>
-    </div>
-</div>
+<!--<div data-thumb="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg"-->
+<!--     data-src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/slid3.jpg">-->
+<!--    <div class="camera_caption">-->
+<!--        <div class="container" style="display:none; opacity: 0; filter: alpha(opacity=0);">-->
+<!--            <div class="eight columns" style="text-align: left; height: 100%;">-->
+<!---->
+<!--                <div class="title des_moveFromLeft"><h1>Customize colors, backgrounds, fonts... Style every page-->
+<!--                    individually!</h1></div>-->
+<!---->
+<!--                <div class="text des_moveFromRight">Build your page with an incredible ease using the Freshlook Page-->
+<!--                    Options, where you can customize every page individually, like adding Slides, change-->
+<!--                    backgrounds… you name it!.-->
+<!--                </div>-->
+<!---->
+<!--                <a class="button medium yellow des_moveFromRight"-->
+<!--                   href="http://themeforest.net/item/freshlook-responsive-multipurpose-wp-theme/3411374?ref=Designare">Like-->
+<!--                    this theme?</a>-->
+<!--            </div>-->
+<!---->
+<!--            <div class="eight columns" style="height: 100%;">-->
+<!--                <img class="image des_moveFromLeft"-->
+<!--                     src="http://designarethemes.com/themes/freshlookwp/wp-content/uploads/2012/11/fresh-colors-1.png"-->
+<!--                     alt="">-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
